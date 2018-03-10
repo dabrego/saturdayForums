59441bacf80f0b577f869f946b287892f42f936b
domingo abrego <dabrego@gmail.com>
2018-03-10 12:32:20 -0500

    Cleanup server project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  // res.render('index', { title: 'Express' });
+
+  res.json({
+    data: "hello world by mingo",
+  });
+
+});
+
+router.get('/about', (req, res) => {
+  // throw new Error(" crashed app ");
+  res.json({
+    name: 'forum-app',
+    version: '0.1.0',
+  });
+
 });
 
 module.exports = router;
